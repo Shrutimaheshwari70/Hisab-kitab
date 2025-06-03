2b517f0d5e2fb3e53a978587a901c7f176d1daa6
khushi0804 <kg830574@gmail.com>
2025-06-03 17:38:02 +0530

    first commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/create", (req, res) => {
 
 
 app.get("/edit/:filename", (req, res) => {
-  fs.readFile(`hisaab/${req.params.filename}`, "utf-8", function (filedata, err) {
+  fs.readFile(`hisaab/${req.params.filename}`, "utf-8", function (err, filedata) {
     if (err) {
       return res.status(500).send(err)
     } else {
@@ -36,4 +36,49 @@ app.get("/edit/:filename", (req, res) => {
     }
   })
 })
+
+app.post("/update/:filename", (req, res) => {
+  fs.writeFile(`./hisaab/${req.params.filename}`, req.body.content, function (err) {
+    if (err) {
+      return res.status(500).send(err)
+    }
+    else {
+      res.redirect("/")
+    }
+  })
+})
+
+app.get("/hisaab/:filename", function (req, res) {
+  fs.readFile(`./hisaab/${req.params.filename}`, "utf-8", function (err, filedata) {
+    if (err) {
+      return res.status(500).send(err)
+    } else {
+      res.render("hisaab", { filedata, filename: req.params.filename })
+    }
+  })
+})
+
+
+app.get("/delete/:filename", (req, res) => {
+  fs.unlink(`./hisaab/${req.params.filename}`, function (err) {
+    if (err) {
+      return res.status(500).send(err)
+    } else {
+      res.redirect("/")
+    }
+  })
+})
+app.post("/createhisaab", (req, res) => {
+  // let currentdate = new Date();
+  // let date = `${currentdate.getDate()}  - ${currentdate.getMonth() + 1} - ${currentdate.getFullYear()}`
+  fs.writeFile(`./hisaab/${req.body.title}.txt`, req.body.content, function (err) {
+    if (err) {
+      return res.status(500).send(err)
+    } else {
+      res.redirect("/")
+    }
+  })
+})
+
+
 app.listen(5500)
